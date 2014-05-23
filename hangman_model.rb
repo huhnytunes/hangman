6abc10d113a02b64ea4f66c4e0e488def5bfbc90
Tim <timothyjmcclung@gmail.com>
2014-05-23 16:33:42 -0400

    add word and letterbank classes, took strain off controller

--- hangman_model.rb
+++ hangman_model.rb
@@ -12,10 +12,48 @@ class WordBank
   end
 
   def get_word(size)
-    @word_hash[size].sample.downcase
+    @word_hash[size].sample.upcase
   end
 
   def random_word
     get_word(rand(4..24))
+  end
+end
+
+class Word
+  attr_reader :target_word, :current_word
+  def initialize(word)
+    @target_word = word.upcase.split('')
+    @current_word = []
+    @target_word.length.times {@current_word << "_"}
+  end
+
+  def check_word(letter)
+    letter.upcase!
+    indices = []
+    @target_word.each_with_index{|space, index| indices << index if space == letter}
+    indices.each {|index|@current_word[index] = @target_word[index]}
+    @current_word
+  end
+
+  def is_correct?(letter)
+    letter.upcase!
+    @target_word.include? letter
+  end
+end
+
+class LetterBank
+  attr_reader :correct_letters, :incorrect_letters
+  def initialize
+    @correct_letters = []
+    @incorrect_letters = []
+  end
+
+  def add_correct(letter)
+    @correct_letters << letter
+  end
+
+  def add_incorrect(letter)
+    @incorrect_letters << letter
   end
 end

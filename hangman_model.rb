43d4d6449de85fdfba6ec95809d5e7131d985d57
Tim <timothyjmcclung@gmail.com>
2014-05-23 15:26:11 -0400

    refactor

--- hangman_model.rb
+++ hangman_model.rb
@@ -1,42 +1,27 @@
 class WordBank
-  attr_reader :word_array
   def initialize
-    @word_array = []
     load_words
   end
 
   def load_words
+    @word_hash = {}
     @words = File.open('word_bank.csv')
     @words.each_line do |word|
-      @word_array << word.chomp
-    end
-    @words.close
-    csv_to_hash
-  end
-
-  def csv_to_hash
-    @word_hash = {}
-    @word_array.each do |word|
-      word_length = word.size              # "gyne" => word
+      word_length = word.chomp.size
       if @word_hash[word_length].is_a?(Array)
-        @word_hash[word_length] << word
+        @word_hash[word_length] << word.chomp
       else
-        @word_hash[word_length] = [word]
+        @word_hash[word_length] = [word.chomp]
       end
     end
+    @words.close
   end
 
   def get_word(size)
-    @word_hash[size].sample
+    @word_hash[size].sample.downcase
   end
 
   def random_word
     get_word(rand(4..24))
   end
 end
-
-words = WordBank.new
-p words.random_word
-# words.load_words
-# p words.word_array
-#p words.random_word
